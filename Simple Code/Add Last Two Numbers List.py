d49f9ccb91e4cec1536f90682b7c7282862cd195
CaptainMK25 <90539589+CaptainMK25@users.noreply.github.com>
2022-01-18 14:55:10 -0500

    Update Add Last Two Numbers List.py

--- Simple Code/Add Last Two Numbers List.py
+++ Simple Code/Add Last Two Numbers List.py
@@ -1,16 +1,24 @@
-def series(n):
-    n1 = 0
-    n2 = 1
-    count = 0
-    if n <= 0:
-        print('Please enter a positive integer')
-        n = int(input('Enter a number here: '))
-    elif n >= 0:
-        while count < n:
-            print(n1)
-            nth = n1 + n2
-            n1 = n2
-            n2 = nth
-            count = count + 1
+def series():
+    while True:
+        try:
+            n = int(input("Enter a positive number here: "))
+            n1 = 0
+            n2 = 1
+            count = 0
 
-series(120)
+            if n == 0:
+                return 0
+            elif n == 1:
+                return 1
+            else:
+                while count < n - 1:
+                    nth = n1 + n2
+                    n1 = n2
+                    n2 = nth
+                    count += 1
+            return nth
+            break
+        except:
+            print("Please enter a positive number")
+
+print(series())
